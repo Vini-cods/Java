3a7d90d3cec331ff95ec9232f5171fe5c40294dc
Vini-cods <vinisanto141@gmail.com>
2025-05-24 17:10:37 -0300

    feat: primeira aula de java

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+//        String nome = "Vinícius";
+
+        int valor1 = 10;
+        int valor2 = 23;
+        int valor3 = 12;
+        int valor4 = 13;
+        int valor5 = 90;
+        double valor6 = 100;
+        double valor7 = 150;
+
+        System.out.println(valor1 + valor2);
+        System.out.println(valor2 - valor3);
+        System.out.println(valor4 / valor1);
+        System.out.println(valor5 * valor2);
+        System.out.println(valor5 % valor2);
+        System.out.println(valor6 % valor7);
+        System.out.println(valor7 * valor6);
+
+//        System.out.println(nome);
+
+
+        String letra = "assadasdswd";
+        int sup = 999999999;
+        Long sup2 = 9999999999999999L;
+        float sup3 = 99999999999999999999999999999999999999.9F;
+        double sup4 = 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999.9;
+
+    }
+}
